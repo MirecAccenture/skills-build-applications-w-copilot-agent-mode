974ac953623b6aba454597d3945f7964c2f6fbd9
MirecAccenture <miroslav.voltemar@accenture.com>
2026-01-02 13:40:54 +0000

    Register users app, update core_app, and commit migrations

--- octofit-tracker/backend/octofit_tracker/core_app/models.py
+++ octofit-tracker/backend/octofit_tracker/core_app/models.py
@@ -2,8 +2,24 @@ from django.db import models
 
 
 # Placeholder model for the core app
-class Example(models.Model):
-    name = models.CharField(max_length=100)
+from octofit_tracker.accounts.models import CustomUser
 
-    def __str__(self):
-        return self.name
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    members = models.ManyToManyField(CustomUser, related_name='teams')
+
+class Activity(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()  # minutes
+    date = models.DateField()
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    suggested_for = models.ManyToManyField(CustomUser, related_name='suggested_workouts')
+
+class Leaderboard(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
